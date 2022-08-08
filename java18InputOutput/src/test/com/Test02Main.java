f1ee3bb1955a06f4f63c637d47d593fc8ab6f78d
darkblose <darkblose@gmail.com>
2022-08-08 16:54:44 +0900

    Day10_20220808

--- java18InputOutput/src/test/com/Test02Main.java
+++ java18InputOutput/src/test/com/Test02Main.java
@@ -3,13 +3,13 @@ package test.com;
 import java.io.*;
 import java.net.URL;
 
-public class Test01Main {
+public class Test02Main {
     public static void main(String[] args) {
         System.out.println("Hello io");
 
-        Class<?> tm_clazz = Test01Main.class;
-        URL url = tm_clazz.getResource("language.txt");
-        String path = url.getPath();
+//        Class<?> tm_clazz = Test02Main.class;
+//        URL url = tm_clazz.getResource("language.txt");
+        String path = "C:\\Projects\\IdeaProjects\\golfzon\\javaStudy\\java18InputOutput\\txt\\language.txt";
         System.out.println(path);
 
         Reader fr = null;
