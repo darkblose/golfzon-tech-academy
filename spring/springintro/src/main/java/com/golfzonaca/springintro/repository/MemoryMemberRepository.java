8e64c13065df3c491953a5b6f09326686753e73c
darkblose <darkblose@gmail.com>
2022-10-06 20:02:01 +0900

    Spring Core Basic : Day01

--- spring/springintro/src/main/java/com/golfzonaca/springintro/repository/MemoryMemberRepository.java
+++ spring/springintro/src/main/java/com/golfzonaca/springintro/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package com.golfzonaca.springintro.repository;
 
 import com.golfzonaca.springintro.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
